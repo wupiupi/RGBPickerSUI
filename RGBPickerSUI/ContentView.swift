cfa10082abd6cd15d2785796355be9cb2839517a
Paul <pasha.makey@gmail.com>
2024-03-05 18:59:14 +0300

    🔧 chore: sliders extracted to separate struct

--- RGBPickerSUI/ContentView.swift
+++ RGBPickerSUI/ContentView.swift
@@ -8,10 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: - @State Properties
     @State private var redSliderValue = Double.random(in: 0...255)
     @State private var greenSliderValue = Double.random(in: 0...255)
     @State private var blueSliderValue = Double.random(in: 0...255)
 
+    // MARK: - Body
     var body: some View {
         VStack(spacing: 16) {
             RoundedRectangle(cornerRadius: 30)
@@ -23,21 +25,15 @@ struct ContentView: View {
                         blue: blueSliderValue / 255
                     )
                 )
-                .overlay(RoundedRectangle(cornerRadius: 30).stroke(.white, lineWidth: 5))
-            HStack {
-                Text(redSliderValue.string())
-                Slider(value: $redSliderValue, in: 0...255, step: 1)
-                    .tint(.red)
-            }
-            HStack {
-                Text(greenSliderValue.string())
-                Slider(value: $greenSliderValue, in: 0...255, step: 1)
-                    .tint(.green)
-            }
-            HStack {
-                Text(blueSliderValue.string())
-                Slider(value: $blueSliderValue, in: 0...255, step: 1)
-            }
+                .overlay(
+                    RoundedRectangle(cornerRadius: 30)
+                        .stroke(.white, lineWidth: 5)
+                )
+            
+            SetSlider(sliderValue: $redSliderValue, tintColor: .red)
+            SetSlider(sliderValue: $greenSliderValue, tintColor: .green)
+            SetSlider(sliderValue: $blueSliderValue, tintColor: .blue)
+            
             Spacer()
         }
         .padding()
@@ -48,8 +44,16 @@ struct ContentView: View {
     ContentView()
 }
 
-extension Double {
-    func string() -> String {
-        String(format: "%.f", self)
+struct SetSlider: View {
+    @Binding var sliderValue: Double
+    let tintColor: Color
+    
+    var body: some View {
+        HStack {
+            Text(sliderValue.string())
+            Slider(value: $sliderValue, in: 0...255, step: 1)
+                .tint(tintColor)
+        }
     }
 }
+
