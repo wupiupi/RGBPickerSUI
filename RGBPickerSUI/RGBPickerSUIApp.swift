a29a23b4edb62c48ad10e02276cafb431b168149
Paul <pasha.makey@gmail.com>
2024-03-05 17:56:46 +0300

    Initial Commit

---
+++ RGBPickerSUI/RGBPickerSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBPickerSUIApp.swift
+//  RGBPickerSUI
+//
+//  Created by Paul Makey on 5.03.24.
+//
+
+import SwiftUI
+
+@main
+struct RGBPickerSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
